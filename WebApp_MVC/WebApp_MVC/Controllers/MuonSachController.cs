8f3514b6e8b2630d1fa8ef3e34b8485ade52816b
ThachLinh <nlinh6894@gmail.com>
2016-10-24 13:23:56 +0700

    .....

---
+++ WebApp_MVC/WebApp_MVC/Controllers/MuonSachController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_MVC.Controllers
+{
+    public class MuonSachController : Controller
+    {
+        // GET: MuonSach
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TimKiemDocGia( string ma, string ten)
+        {
+            return View("/Home/MuonSach");
+        }
+        [HttpPost]
+        public ActionResult LuuPhieuMuon(FormCollection form)
+        {
+            return View();
+        }
+        public ActionResult timkiemsach(string ma, string ten)
+        {
+            return View(); 
+        }
+
+    }
+}
